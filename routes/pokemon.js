21c3a27376bf7bf8e17cbca839923f8f739447b0
Alan <aespinosa45@alumnos.uaq.mx>
2023-09-24 16:07:03 -0600

    added json format for responses

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,42 +3,32 @@ const pokemon = express.Router();
 const DB = require('../config/database')
 
 pokemon.post('/',(req, res, next) => {
-    return res.status(200).send(req.body.name);
+    return res.status(200).json(req.body);
 })
 
 pokemon.get('/', async(req, res, next) => {
-    const consult = await DB.query('SELECT * FROM pokemon');
-    return res.status(200).json(consult);
+    const consult = await DB.query("SELECT * FROM pokemon");
+    return res.status(200).json({ code: 1, message: consult});
 })
 
-pokemon.get('/:id([0-9]{1,3})',(req, res, next) => {
+pokemon.get('/:id([0-9]{1,3})',async(req, res, next) => {
     const id = req.params.id - 1;
     if (id >= 0 && id <= 150) {
-        return res.status(200).send(DB[req.params.id -1]);
+        const consult = await DB.query("SELECT * FROM pokemon WHERE pok_id = '" + id +"' ;");
+        return res.status(200).json({ code: 1, message: consult});
     }
-    return res.status(404).send('Pokemon no encontrado');
+    return res.status(404).json({ code: 404, message: "Pokemon no encontrado" });
 })
 
-pokemon.get('/:name([A-Za-z]+)',(req, res, next) => {
+pokemon.get('/:name([A-Za-z]+)',async(req, res, next) => {
     const name = req.params.name;
-    
-    const search = DB.filter((result) =>{
-        return (result.name.toUpperCase() == name.toUpperCase()) && result;
-    });
 
-    if (search.length > 0) {
-        return res.status(200).send(search);
+    const consult = await DB.query("SELECT * FROM pokemon WHERE pok_name LIKE '" + name +"' ;");
+
+    if (consult.length > 0) {
+        return res.status(200).json({ code: 1, message: consult});
     }    
-    return res.status(404).send('Pokemon no encontrado');
-    /* 
-    for (i = 0; i < pokemon.length; i++) {
-        
-        if (pokemon[i].name.toUpperCase() == name.toUpperCase()) {
-            return res.status(200).send(pokemon[i])
-        }
-        
-    } 
-    */    
+    return res.status(404).json({ code: 404, message: "Pokemon no encontrado" }); 
 })
 
 module.exports = pokemon;
