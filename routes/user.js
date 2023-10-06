29bc4e0bb474b8173e41ad2e1278bededa271b49
Alan <aespinosa45@alumnos.uaq.mx>
2023-10-05 22:30:41 -0600

    added user login template

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const user = express.Router();
-const DB = require('../config/database')
+const DB = require('../config/database');
+const jwt = require('jsonwebtoken');
 
 user.post('/', async(req, res, next) => {
     const { user_name, user_mail, user_password} = req.body;
@@ -14,6 +15,27 @@ user.post('/', async(req, res, next) => {
             return res.status(201).json({ code: 201, message: "Usuario insertado correctamente"});
         }
         return res.status(500).json({ code: 500, message: "Ocurrio un error"});
+    }
+    return res.status(500).json({ code: 500, message: "Capos incompletos"});
+})
+
+user.post('/login', async (req, res, next) => {
+    const {user_mail, user_password} = req.body;
+    
+    if (user_mail && user_password ) {
+        const query = `SELECT * FROM users WHERE user_mail = '${user_mail}' AND user_password = '${user_password}'`;
+
+        const rows = await DB.query(query)
+
+        if (rows.length == 1) {
+            const token = jwt.sign({
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail,
+            }, "debugkey");
+
+            return res.status(201).json({ code: 201, message: token});
+        }
+        return res.status(401).json({ code: 401, message: "Registro no encontrado"});
     }
     return res.status(500).json({ code: 500, message: "Capos incompletos"});
 })
